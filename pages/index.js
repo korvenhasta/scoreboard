0c43a1f617ab7140016b22e43e319ef70f317b5e
Marina Kenina <avrora-lisa@yandex.ru>
2022-11-21 13:33:53 +0100

    added functions +50% and -50%

--- pages/index.js
+++ pages/index.js
@@ -16,17 +16,65 @@ export default function Home() {
 
   let [counter, setCounter] = useState(initialState);
 
+  // Reset score
+  function resetScore() {
+    setCounter(initialState)
+  } 
 
-  function increaseScore(){
-    setCounter(counter + 1)
+  // Display message
+  function displayMessage(score) {
+    if (score === 0) {
+      return "A journey of a 1000 miles starts with single step"
+    }
+    else if (score > 0 &&  score <= 10) {
+      return "C'mon you can do it"
+    }
+    else if (score > 10 &&  score <= 45) {
+      return "You're getting there"
+    }
+    else {
+      return "Hold on! Finish strong"
+    }
   }
 
-  function decreaseScore(){
+  function decreaseScoreByOne(){
     setCounter(counter - 1)
   }
 
-  function resetScore() {
-    setCounter(initialState)
+  function increaseScoreByFive(){
+    setCounter(counter + 5)
+  }
+
+  function decreaseScoreByFive(){
+    setCounter(counter - 5)
+  }
+
+  // Change score
+  function changeScore(changeBy) {
+    console.log("Change by: ", changeBy)
+    setCounter(counter + changeBy)
+  }
+
+  // +50%
+  function addFiftyPercent() {
+    let res = Math.round(counter * 1.5);
+    if (res >= 100) {
+      setCounter(counter)
+    }
+    else {
+      setCounter(res)
+    }
+  }
+
+  // -50%
+  function minusFiftyPercent() {
+    let res = Math.round(counter * 0.5);
+    if (res <= 0) {
+      setCounter(counter)
+    }
+    else {
+      setCounter(res)
+    }
   }
 
   return (
@@ -43,28 +91,30 @@ export default function Home() {
         </Title>
 
         <Label></Label>
-        <Progress value={counter} max="100"></Progress>
+        <Progress value={counter} max="100"></Progress> 
+
+        {/* style={progressBarColor(counter)} */}
 
         <ButtonContainer>
-          <Paragraph>A journey of a 1000 miles starts with single step</Paragraph>
+          <Paragraph>{displayMessage(counter)}</Paragraph>
           <Paragraph>{counter} / 100</Paragraph>
         </ButtonContainer>
 
 
         <ButtonContainer>
-          <Button>-50%</Button>
+          <Button onClick={minusFiftyPercent} disabled={counter <= 0}>-50%</Button>
 
-          <Button>-5</Button>
+          <Button onClick={decreaseScoreByFive} disabled={counter <= 0}>-5</Button>
 
-          <Button onClick={decreaseScore} disabled={counter <= 0}>-1</Button>
+          <Button onClick={decreaseScoreByOne} disabled={counter <= 0}>-1</Button>
 
-          <Button onClick={resetScore}>Reset</Button>
+          <Button onClick={resetScore} disabled={counter === 0}>Reset</Button>
 
-          <Button onClick={increaseScore}>+1</Button>
+          <Button onClick={() => changeScore(1)} disabled={counter === 100}>+1</Button>
 
-          <Button>+5</Button>
+          <Button onClick={increaseScoreByFive} disabled={counter === 100}>+5</Button>
 
-          <Button>+50%</Button>
+          <Button onClick={addFiftyPercent} disabled={counter === 0}>+50%</Button>
         </ButtonContainer>
 
       </main>
